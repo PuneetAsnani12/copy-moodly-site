d835481a31a5ab60299e058c7b91afd124e5ee6b
PuneetAsnani12 <asnani.puneet@gmail.com>
2021-12-07 17:23:53 +0530

    header and footer added, barebones completed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,3 +1,12 @@
-const Footer = () => <div className="Footer"></div>;
+import "./footer.styles.scss"
+
+const Footer = () => (
+  <div className="Footer">
+    <div className="text-wrapper">
+        Made By {"  "}
+      <a id="text-link" href="https://www.instagram.com/_puneetasnani/" target="_blank" rel="noreferrer">@_puneetasnani</a>
+    </div>
+  </div>
+);
 
 export default Footer;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,3 +1,46 @@
-const Header = () => <div className="Header"></div>;
+import "./header.styles.scss";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import avatarImage from "../../assets/images/avatar.jpg";
+import instaImage from "../../assets/images/instagram.png";
+
+const Header = () => (
+  <div className="header">
+    <div className="button-wrapper">
+      <a className="background-button" href="#" title="Moodly"></a>
+    </div>
+    <div className="copyright-follow">
+      <a href="https://www.instagram.com/_puneetasnani/" target="_blank" rel="noreferrer" >
+        <Button
+          className="Button"
+          style={{
+            borderRadius: 5,
+            background: "white",
+            textTransform: "none",
+            padding: 0,
+            border: "0.5px solid #ccc",
+          }}
+        >
+          <div className="Button__inner">
+            <div className="avatar">
+              <Avatar
+                alt="@PuneetAsnani12"
+                src={avatarImage}
+                style={{ height: "30px", width: "30px" }}
+              />
+            </div>
+            <div className="text">
+              <div className="text__upper">By Puneet Asnani</div>
+              <div className="text__lower">
+                follow on instagram
+                <img src={instaImage} alt="instagram"></img>
+              </div>
+            </div>
+          </div>
+        </Button>
+      </a>
+    </div>
+  </div>
+);
 
 export default Header;
